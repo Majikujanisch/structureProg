2361461ff8f40c4035a7f668b7df2ebc85a62e33
Anna-Maria Tipotsch c102371 <anna-maria.tipotsch@uibk.ac.at>
2022-12-19 13:46:40 +0100

    logging cleanup

--- server/tools/logging.js
+++ server/tools/logging.js
@@ -1,46 +1,66 @@
-const fs = require ('fs')
-const pD = require('./prettyDate')
-path = './logs/logall.txt'
+const fs = require("fs");
+const pD = require("./prettyDate");
+path = "./logs/logall.txt";
 
-
-function logApi (req, res, type, reason){
-    username = req.body.user
-    console.log(!fs.existsSync(path))
-    if (!fs.existsSync(path)){
-        console.log("If-Abfrage")
-        fs.writeFile(path, "created logging file", err => {
-            if(err){
-                console.log(err)
+function logApi(req, res, type, reason) {
+  username = req.body.user;
+  console.log(!fs.existsSync(path));
+  if (!fs.existsSync(path)) {
+    console.log("If-Abfrage");
+    fs.writeFile(path, "created logging file", (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+  switch (type) {
+    case "registration":
+      switch (reason) {
+        //everything worked fine
+        case "0":
+          var content =
+            "\n" + pD.prettyDate() + " registration " + username + " Worked";
+          fs.appendFile(path, content, (err) => {
+            if (err) {
+              console.error(err);
             }
-        })
-    }
-    switch(type){
-        case 'registration':
-            switch (reason){
-                case '0':
-                    var content = '\n' +  pD.prettyDate() + " registration " + username + " Worked";
-                    fs.appendFile(path, content, err => {
-                    if (err) {
-                        console.error(err);
-                    }
-             });
-             break;
-             case '1':
-                var content = '\n' +  pD.prettyDate()  + " registration " + username +  " Failed due to an existing User with this Username";
-                    fs.appendFile(path, content, err => {
-                    if (err) {
-                        console.error(err);
-                    }
-                });
-                break;
-            default:
-                console.log("Wrong reason")
+          });
+          break;
+
+        //Username is already in Use
+        case "1":
+          var content =
+            "\n" +
+            pD.prettyDate() +
+            " registration " +
+            username +
+            "Failed due to an existing User with this Username";
+          fs.appendFile(path, content, (err) => {
+            if (err) {
+              console.error(err);
             }
-        break
-        default: 
-        console.log("Wrong service input")
-        
-}
+          });
+          break;
+          case "2":
+            var content =
+            "\n" +
+            pD.prettyDate() +
+            " registration " +
+            username +
+            "Failed due to an empty response";
+          fs.appendFile(path, content, (err) => {
+            if (err) {
+              console.error(err);
+            }
+          });
+          break;
+        default:
+          console.log("Wrong reason");
+      }
+      break;
+    default:
+      console.log("Wrong service input");
+  }
 }
 
-module.exports = {logApi}
+module.exports = { logApi };
