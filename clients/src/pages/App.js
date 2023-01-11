d857ea3ad7d51224246a57e6841d5dab137d15b1
Anna-Maria Tipotsch c102371 <anna-maria.tipotsch@uibk.ac.at>
2023-01-11 10:42:36 +0100

    removed old Router and rewrote it in app to better understand and add bootstrap Navigationbar from bootstrap add the Navbar to website and test it with some not rightly assigned components

--- clients/src/pages/App.js
+++ clients/src/pages/App.js
@@ -1,14 +1,26 @@
 
 import './App.css';
-
+import Navbar from '../components/navigationbar'
 import React  from 'react';
-
+import Login from '../pages/login'
+import Regis from '../pages/regristration'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Navbar></Navbar>
+      <Router>
+        <Routes>
+            <Route exact path='/' element={<Login></Login>}/>
+            <Route path='/about' component={Login}/>
+            <Route path='/options' element={<Regis></Regis>}/>
+        </Routes>
+      </Router>
+      
+      
+      <p>Hello World2</p>
+      
     </div>
   );
 }
